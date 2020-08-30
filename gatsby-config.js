3f2be08bb847ed1f806f2cd3a106ae87db7ef726
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2020-08-30 17:56:08 +0100

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `James McEwan`,
+    description: `Art of James McEwan`,
+    author: `@jamesmcewan`,
+    social: [
+      {
+        name: "instagram",
+        url: "https://instagram.com/jamesmcewan",
+      },
+      {
+        name: "mcewan.io",
+        url: "https://mcewan.io",
+      },
+    ],
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+  plugins: [`gatsby-theme-gallery`],
+};
